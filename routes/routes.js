a83cf518b557e7b7bed9628635ad27ebd789e0f5
Matthew Riddle <Matthew.B.Riddle@gmail.com>
2018-08-09 14:36:10 -0500

    In a working state

--- routes/routes.js
+++ routes/routes.js
@@ -4,13 +4,13 @@ const performGetRequest = require("../index")
 
 var appRouter = app => {
   app.get("/", function(req, res) {
-    res.status(200).send({
+    res.send({
       message:
         "Welcome to my Restful API. You can retrieve five followers of a github user along with five of their followers up to 3 levels deep."
     })
   })
 
-  app.get("/user", async (req, res) => {
+  app.get("/:userId", async (req, res) => {
     let state = {
       // State object to model and store our data
       user: "",
@@ -26,39 +26,38 @@ var appRouter = app => {
 
     let isUserFound = true
 
-    await getUser(req.params.user) // Await is necessary with asynchronus calls to make them behave more like a synchronized function
+    await getUser(req.params.userId) // Await is necessary with asynchronus calls to make them behave more like a synchronized function
       // In this case we need to wait for the promise to be fulfilled so we can use the data retrieved or return if nothing is found
-      .then(response => (state.user = response.data.login))
+      .then(response => (state.user = response.data["login"]))
       .catch(error => {
         isUserFound = false
-        res.status().send(error.message)
+        res.send(error.message)
       })
 
-    if (status === false) return
+    if (isUserFound === false) return
 
-    getFollowers(req.params.user).then(response => {
-      extractFollowers(response.data)
+    getFollowers(req.params.userId)
+      .then(response => {
+        return extractFollowers(response.data)
         // .slice(0, 5) // Slice out the first 5 followers.Thank god for transducers
         // .map(val => val.login) // Map the sliced out followers login names to a new array that we will return
-    })
-    .then(async (followers) => {
-      followers.length.forEach(follower => {
-        let temp = {}
-        let ff = await getFollowers(followers[i])
-        .then(response => {
-          extractFollowers(response.data)
-        })
+      })
+      .then(async followers => {
+        for (let i = 0; i < followers.length; i++) {
+          let temp = {}
+          let ff = await getFollowers(followers[i]).then(response => {
+            return extractFollowers(response.data)
+          })
 
-        temp.user = followers[i]
-        temp.followers = ff
-        state.followers.push(temp)
-
-      });
-      res.status().send(state)
-    })
-    .catch(error => {
-      res.status().send(error.message)
-    })
+          temp.user = followers[i]
+          temp.followers = ff
+          state.followers.push(temp)
+        }
+        res.send(state)
+      })
+      .catch(error => {
+        res.send(error.message)
+      })
 
     // let respo = axios
     //   .get("/users/matthew-riddle/followers")
@@ -74,18 +73,16 @@ var appRouter = app => {
     //   })
     //   .catch(error => console.log(error))
   })
-
   getUser = user => {
     return axios.get(`users/${user}`)
   }
 
   getFollowers = user => {
-    return axios.get(`user/${user}/followers`)
+    return axios.get(`users/${user}/followers`)
   }
 
-  extractFollowers = (response) => {
-    response.slice(0, 5)
-    .map(val => val.login)
+  extractFollowers = response => {
+    return response.slice(0, 5).map(val => val["login"])
   }
 }
 
